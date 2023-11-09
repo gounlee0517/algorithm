8750daaa68d4a596f61becb9d10540a3533db71a
leegoun0517 <leegoun517@gmail.com>
2023-11-09 10:13:10 +0900

    daily algorithm

---
+++ 13.자릿수 더하기.js
@@ -0,0 +1,14 @@
+// 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서
+//  return 하는 solution 함수를 만들어 주세요.
+// 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+
+function solution(n) {
+  let newArr = n.toString().split('').map(n);
+  let answer = 0;
+
+  for(let i = 0; i < newArr.length; i++) {
+    answer += newArr[i];
+  }
+
+  return answer;
+}
